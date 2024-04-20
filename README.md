4a37026247297e79d5bbe95530a9070b797d0c31
Jonathanfajardo <jonnyfajardo18@gmail.com>
2024-04-20 13:54:00 -0400

    finished read me, and notes correctly save, display to the left and then back to the right when clicked on, and delete button works as well

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # 11Express-NoteTaker
-An application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+## Description
+
+This is a simple note-taking application built with Node.js and Express.js. It allows users to create, view, and delete notes.
+
+## Features
+
+- Create new notes with a title and text content
+- View existing notes
+- Delete notes
+
+## Installation
+
+To install the application, follow these steps:
+
+1. Clone the repository to your local machine
+2. Navigate to the project directory in the terminal
+3. Run `npm install` to install the necessary dependencies
+
+## Usage
+
+To start the server, run `node server.js` in the terminal. Then, open your web browser and navigate to `http://localhost:3000`.
+
+## Dependencies
+
+- [Express.js](https://expressjs.com/)
+- [uuid](https://www.npmjs.com/package/uuid) for generating unique ids for notes
+
+## Contributing
+
+If you'd like to contribute to this project, please create a fork of the repository and submit a pull request.
+
+## License
+
+This project is licensed under the MIT License.
+
+## Contact
+
+If you have any questions or feedback, please feel free to contact me at
--- server.js
+++ server.js
@@ -10,15 +10,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//html routes
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-
 //API routes
 app.get('/api/notes', (req, res) => {
     fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
+    });
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const note = notes.find(note => note.id === noteId);
+        
+        if (note) {
+            res.json(note);
+        } else {
+            res.status(404).send('Note not found');
+        }
+    });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        notes = notes.filter(note => note.id !== noteId);
+
+        fs.writeFile('./Develop/db/db.json', JSON.stringify(notes, null, 2), (err) => {
+            if (err) throw err;
+            res.send('Note deleted');
+        });
     });
 });
 
@@ -37,5 +64,9 @@ app.post('/api/notes', (req, res) => {
         });
     });
 });
+
+//html routes
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
