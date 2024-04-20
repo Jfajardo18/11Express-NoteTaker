fbd53a2882840d52b08023a1c96e1146e527ad40
Jonathanfajardo <jonnyfajardo18@gmail.com>
2024-04-20 13:16:52 -0400

    saving note functionality is now working, need to be able to see existing notes when clicked though and then add delete button

--- server.js
+++ server.js
@@ -8,29 +8,34 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 //html routes
-app.get('/notes', (req, res) => {
-    fs.readFile('./db.json', 'utf8', (err, data) => {
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+//API routes
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
     });
 });
 
-app.post('/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     const newNote = req.body;
     newNote.id = uuidv4();
 
-    fs.readFile('./db.json', 'utf8', (err, data) => {
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
         notes.push(newNote);
 
-        fs.writeFile('./db.json', JSON.stringify(notes, null, 2), (err) => {
+        fs.writeFile('./Develop/db/db.json', JSON.stringify(notes, null, 2), (err) => {
             if (err) throw err;
             res.json(newNote);
         });
     });
 });
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
